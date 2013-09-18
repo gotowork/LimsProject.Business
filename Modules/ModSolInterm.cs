c0d5675f49331485174d3e4cebe1472d4975a099
gotolims <bvij@outlook.com>
2013-09-17 21:19:12 -0500

    soluciones

--- Modules/ModSolInterm.cs
+++ Modules/ModSolInterm.cs
@@ -17,6 +17,17 @@ namespace LimsProject.BusinessLayer.Modules
                  select m).ToList();
 
             return lst;
-        }        
+        }
+
+        public List<CSolution> GetLstSolution(short? idelement, int type_pattern)
+        {
+            List<CSolution> lst =
+                (from m in new CSolutionFactory().GetAll()
+                 from n in new CGroup_solutionFactory().GetAll().Where(x=> x.Idgroup_solution == m.Idgroup_solution)
+                 where n.Idelement == idelement && n.Type_pattern == type_pattern
+                 select m).ToList();
+
+            return lst;
+        }
     }
 }
